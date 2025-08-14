61f1cdacb76fb111aa747c16871d0ea89eb81fa3
Wheel23 <luiz_santos25@estudante.sesisenai.org.br>
2025-08-14 08:28:02 -0300

    first commit

---
+++ projeto/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TituloPagina from './components/TituloPagina'
+
+
+const App = () => {
+  const produtoA = {
+
+  }
+  return (
+    <div>
+      <TituloPagina />
+    </div>
+  )
+}
+
+export default App
---
+++ projeto/src/components/TituloPagina.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TituloPagina = () => {
+  return (
+    <div>
+        <h1>Título da Página!</h1>
+    </div>
+  )
+}
+
+export default TituloPagina
